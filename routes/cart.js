5022c5891fbf11c41e7e752df19a95fbabd2fa17
Tabassumm Shaikh <tabassumsk720@gmail.com>
2025-09-18 17:22:41 +0530

    booking function working

--- routes/cart.js
+++ routes/cart.js
@@ -9,4 +9,20 @@ router.get("/", isLoggedIn, async (req, res) => {
     res.render("listings/cart.ejs", { bookedListings });
 });
 
+// Remove a booked listing from cart (set booked to false)
+router.post("/:id/remove", isLoggedIn, async (req, res) => {
+    const { id } = req.params;
+    const listing = await Listing.findById(id);
+    if (!listing) {
+        req.flash("error", "Listing not found!");
+        return res.redirect("/cart");
+    }
+    listing.booked = false;
+    await listing.save();
+    req.flash("success", "Listing removed from cart!");
+    res.redirect("/cart");
+});
+
+
+
 module.exports = router;
